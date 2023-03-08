46b049232cce2089730dd768e170ca366869af09
Ysabel Katchy <ykatchy@gmail.com>
2023-03-08 21:09:05 +0100

    Bug fix

--- Assets/Scripts/GameJamScripts/ScriptableObjectsScripts/EnemyManager.cs
+++ Assets/Scripts/GameJamScripts/ScriptableObjectsScripts/EnemyManager.cs
@@ -4,19 +4,16 @@ using System.Collections;
 using System.Collections.Generic;
 // Date updated: " + DateTime.Now.ToString("MM/dd/yyyy")
 [CreateAssetMenu(fileName = "Managers/EnemyManager", menuName = "EnemyManager")]
-public class EnemyManager : ScriptableObject
+public class EnemyManager : MonoBehaviour
 {
     [FancyHeader("  Enemy Manager  ", 1.5f, "blue", 5.5f, order = 0)]
-    [SerializeField] List<EnemySO> charObjects;
-    
-    [SerializeField] int charIndex;
-    
-    [SerializeField]CharacterUnityEvent _mcharacterevent;
-    public void UpdateCharacter()
+    [SerializeField] EnemySO enemyObjects;
+    [SerializeField] Transform[] spwanPoints;
+    public void SpawnEnemy()
     {
-        for (int i = 0; i < charObjects.Count; i++)
-        {
-            //_mcharacterevent.Raise(charObjects[i]);
-        }
+       foreach (Transform _spwanPoint in spwanPoints)
+       {
+            Instantiate(enemyObjects._enemyPrefab, _spwanPoint.position, Quaternion.identity,transform);
+       }
     }
 }
