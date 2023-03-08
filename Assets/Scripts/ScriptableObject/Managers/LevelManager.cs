76a962cd971e14c0cd333454a9327f7242f91000
Nwoga Kizito <63211231+kizito-cyber@users.noreply.github.com>
2023-03-08 14:48:56 +0100

    Level Manager and Player Manager

--- Assets/Scripts/ScriptableObject/Managers/LevelManager.cs
+++ Assets/Scripts/ScriptableObject/Managers/LevelManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LevelManager", menuName = "Managers/Level Manager")]
 public class LevelManager : ScriptableObject
 {
-
-    
-
+      [FancyHeader("  Level Manager  ", 1.5f, "blue", 5.5f, order = 0)]
+        public LevelSO[] LevelSO;
+         [SerializeField] int charIndex;
   
 }
